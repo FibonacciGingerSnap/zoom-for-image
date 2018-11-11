31191c50130f4224770eb7284bd53594c1600a3d
Mozhega Alexandr <mozhega@rozetka.com.ua>
2018-11-11 17:40:29 +0200

    add zooming effect

--- zoom/src/app/image/image.component.ts
+++ zoom/src/app/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { images } from '../../product';
 
 @Component({
@@ -8,11 +8,42 @@ import { images } from '../../product';
 })
 export class ImageComponent implements OnInit {
   photos = images;
-
+  selected = this.photos[0];
+  Lens = { 'width.px' : 0 , 'height.px' : 0, 'left.px' : 0, 'top.px' : 0 };
+  Zoom = { on: false, scaleX: 0, scaleY: 0, 'width.px' : 0 , 'height.px' : 0 , 'backgroundImage' : 'url(' + this.selected.original + ')' };
+  @ViewChild('photo') img;
+  @ViewChild('zoom') zoom;
   constructor() { }
 
+  select(x) {
+    this.selected = x;
+  }
+  zooming () {
+    const F = this.img.nativeElement.getBoundingClientRect();
+    this.Zoom.on = true;
+    this.Zoom.scaleX = this.Zoom['width.px'] / this.selected.width;
+    this.Zoom.scaleY = this.Zoom['height.px'] / this.selected.height;
+    this.Zoom['width.px'] = this.zoom.nativeElement.clientWidth;
+    this.Zoom['height.px'] = this.zoom.nativeElement.clientHeight;
+    console.log(this.Zoom );
+    this.Lens['width.px'] = (F.width * this.Zoom.scaleX);
+    this.Lens['height.px'] = (F.height * this.Zoom.scaleY);
+    console.log(this.Lens);
+  }
+  getCursorPos(e) {
+    let x: number, y: number;
+    const F = this.img.nativeElement.getBoundingClientRect();
+    x = e.pageX - F.left;
+    y = e.pageY - F.top;
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    this.Lens['top.px'] = y;
+    this.Lens['left.px'] = x;
+    this.Zoom['backgroundPosition'] = -(x / this.Zoom.scaleX) + 'px -' + (y / this.Zoom.scaleY) + 'px';
+    console.log(this.Lens);
+  }
   ngOnInit() {
-    console.log (this.photos);
+    console.log (this.img);
   }
 
 }
--- zoom/src/product.ts
+++ zoom/src/product.ts
@@ -1,6 +1,48 @@
-export const images = [ {preview: 'https://i2.rozetka.ua/goods/1641787/triumph_7613141551275_images_1641787291.jpg', big: 'https://i1.rozetka.ua/goods/1641787/triumph_7613141551275_images_1641787018.jpg', original : 'https://i1.rozetka.ua/goods/1641786/triumph_7613141551275_images_1641786983.jpg', id:1, width: 2080  , height : 3000},
-  {preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg', big: 'https://i1.rozetka.ua/goods/1641786/triumph_7613141551275_images_1641786983.jpg', original : 'https://i1.rozetka.ua/goods/1641786/triumph_7613141551275_images_1641786983.jpg', id:2, width: 2080  , height :3000},
-  {preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg', big: '', original : '', id:1, width: 0  , height :0},
-  {preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg', big: '', original : '', id:1, width: 0 , height :0},
-  {preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg', big: '', original : '', id:1, width: 0 , height :0},
-  {preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg', big: '', original : '', id:1, width:0  , height :0}];
+export const images = [{
+  preview: 'https://i2.rozetka.ua/goods/1641787/triumph_7613141551275_images_1641787291.jpg',
+  big: 'https://i1.rozetka.ua/goods/1641787/triumph_7613141551275_images_1641787018.jpg',
+  original: 'https://i1.rozetka.ua/goods/1641786/triumph_7613141551275_images_1641786983.jpg',
+  id: 1,
+  width: 2080,
+  height: 3000
+},
+  {
+    preview: 'https://i2.rozetka.ua/goods/4479852/triumph_7613136948431_images_4479852120.jpg',
+    big: 'https://i1.rozetka.ua/goods/4479851/triumph_7613136948431_images_4479851696.jpg',
+    original: 'https://i2.rozetka.ua/goods/4479851/triumph_7613136948431_images_4479851624.jpg',
+    id: 2,
+    width: 2080,
+    height: 3000
+  },
+  {
+    preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg',
+    big: '',
+    original: '',
+    id: 1,
+    width: 0,
+    height: 0
+  },
+  {
+    preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg',
+    big: '',
+    original: '',
+    id: 1,
+    width: 0,
+    height: 0
+  },
+  {
+    preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg',
+    big: '',
+    original: '',
+    id: 1,
+    width: 0,
+    height: 0
+  },
+  {
+    preview: 'https://i2.rozetka.ua/goods/1641789/triumph_7613141551275_images_1641789118.jpg',
+    big: '',
+    original: '',
+    id: 1,
+    width: 0,
+    height: 0
+  }];
